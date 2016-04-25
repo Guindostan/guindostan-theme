8f2c580eeb293f74241739c4232da15cc6a88dc6
xaviju <xaviju@gmail.com>
2016-04-25 20:12:45 +0200

    Redesign groups

--- src/template-parts/groups-loop.php
+++ src/template-parts/groups-loop.php
@@ -1,6 +1,7 @@
-<article class="group-wrapper" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article class="group-wrapper <?php echo $post->post_name;?>" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<a href="<?php the_permalink(); ?>" title="the_title();">
 		<img src="<?php echo get_template_directory_uri(); ?>/img/icons/<?php echo $post->post_name;?>.svg" alt="<?php the_title(); ?>" />
 		<?php the_title( sprintf( '<h2 class="entry-title">', esc_url( get_permalink() ) ), '</h2>' ); ?>
 	</a>
+	<hr>
 </article><!-- #post-## -->
